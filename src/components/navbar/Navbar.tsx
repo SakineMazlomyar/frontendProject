fec5bcf8846ee5203813bdad77809553289ed3c3
sakine <mazlomyar12@gmail.com>
2021-03-21 07:43:27 +0100

    minimum changes

--- src/components/navbar/Navbar.tsx
+++ src/components/navbar/Navbar.tsx
@@ -1,16 +1,15 @@
 import React from 'react'
 import { NavLink } from 'react-router-dom'
 import './Navbar.scss'
-export default class  Navbar  extends React.Component <{}, {}>{
-    render(){
-     
-        return (
-            <nav className="navbar">
-                <ul className="navbarItemContainer ">
-                    <li className="navbarItem"> <NavLink exact to="/" activeClassName="active">Home</NavLink></li>
-                    <li className="navbarItem"><NavLink exact to="/nextpage" activeClassName="active" >Nextpage</NavLink></li>
-                </ul>
-            </nav>)
 
-    }
+const Navbar = ()=> {
+
+    return (
+        <nav className="navbar">
+            <ul className="navbarItemContainer ">
+                <li className="navbarItem"> <NavLink exact to="/" activeClassName="active">Home</NavLink></li>
+                <li className="navbarItem"><NavLink exact to="/nextpage" activeClassName="active" >Nextpage</NavLink></li>
+            </ul>
+        </nav>) 
 }
+export default Navbar
