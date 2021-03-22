dfdde9a0338120080bea16146329df337afa2714
sakine <mazlomyar12@gmail.com>
2021-03-22 16:06:35 +0100

    types

--- src/components/buttons/ButtonInterfaces.tsx
+++ src/components/buttons/ButtonInterfaces.tsx
@@ -1,7 +1,7 @@
 /* Button interfaces  */
 
 export interface ButtonInterface {
-    onclick: () => void,
+    onclick: (event: React.MouseEvent<HTMLButtonElement, MouseEvent>) => void,
     label: string,
     color: string
 }
